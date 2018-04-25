7b8d6ac6206668b4c18da99a7a2dd2925fe43f86
dpyzo0o <yaxu@student.ethz.ch>
2018-04-25 17:01:12 +0200

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgURL" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false
+        }
+      },
+      swiperList: [{
+        id: '0001',
+        imgURL: 'http://img1.qunarzz.com/piao/fusion/1804/14/21f410d1016c8702.jpg_640x200_84d0effe.jpg'
+      }, {
+        id: '0002',
+        imgURL: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_640x200_e1b76963.jpg'
+      }, {
+        id: '0003',
+        imgURL: 'http://img1.qunarzz.com/piao/fusion/1804/2c/4a64ed2e384e0b02.jpg_640x200_da6dfd87.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background: #eee;
+}
+
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.swiper-img {
+  width: 100%;
+}
+</style>
