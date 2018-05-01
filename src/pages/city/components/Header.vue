70a1839339ef9e37b73dd28eceee61c767d5d6e5
dpyzo0o <yaxu@student.ethz.ch>
2018-05-02 00:11:13 +0200

    add city header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style scoped>
+.header {
+  line-height: var(--header-height);
+  text-align: center;
+  color: #fff;
+  background: var(--bg-color);
+  font-size: .32rem;
+}
+
+.back-icon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: .64rem;
+  text-align: center;
+  font-size: .4rem;
+  color: #fff;
+}
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -26,7 +28,7 @@ export default {
 <style scoped>
 .header {
   display: flex;
-  line-height: .86rem;
+  line-height: var(--header-height);
   background: var(--bg-color);
   color: #fff;
 }
@@ -57,6 +59,7 @@ export default {
   width: 1.24rem;
   float: right;
   text-align: center;
+  color: #fff;
 }
 
 .header-right .arrow-icon {
